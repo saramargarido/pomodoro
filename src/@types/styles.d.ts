25840bcdc17bc09222616e2042c08616355972bb
saramargarido <saramargarido@gmail.com>
2024-10-11 18:15:23 +0200

    ESLint config

--- src/@types/styles.d.ts
+++ src/@types/styles.d.ts
@@ -1,9 +1,12 @@
-import 'styled-components';
-import { defaultTheme } from '../styles/themes/default';
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/default'
 
-type ThemeType = typeof defaultTheme;
+type ThemeType = typeof defaultTheme
 
-//Sobrescreve tipagem do Styled component para facilitar a implementação de acordo com o Theme a ser utilizado
+/*
+Sobrescreve tipagem do Styled component para facilitar a
+implementação de acordo com o Theme a ser utilizado
+*/
 declare module 'styled-components' {
   // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   export interface DefaultTheme extends ThemeType {}
