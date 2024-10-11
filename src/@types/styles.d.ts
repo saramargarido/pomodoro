80174ae9afbd2ddf99123241e986de822f4bdd06
saramargarido <saramargarido@gmail.com>
2024-10-11 17:23:25 +0200

    typing themes

---
+++ src/@types/styles.d.ts
@@ -0,0 +1,10 @@
+import 'styled-components';
+import { defaultTheme } from '../styles/themes/default';
+
+type ThemeType = typeof defaultTheme;
+
+//Sobrescreve tipagem do Styled component para facilitar a implementação de acordo com o Theme a ser utilizado
+declare module 'styled-components' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
+  export interface DefaultTheme extends ThemeType {}
+}
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
 
-    </>
+    </ThemeProvider>
   )
 }
 
