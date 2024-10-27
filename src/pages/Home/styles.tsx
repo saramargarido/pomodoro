c83b288878bce7fff7d6f7c41ca307fa07d1d585
saramargarido <saramargarido@gmail.com>
2024-10-27 19:50:47 +0100

    button and form styles

--- src/pages/Home/styles.tsx
+++ src/pages/Home/styles.tsx
@@ -37,6 +37,15 @@ const BaseInput = styled.input`
   font-size: 1.125rem;
   padding: 0 .5rem;
   color: ${(props) => props.theme['gray-100']};
+
+  &:focus {
+    box-shadow: none;
+    border-color: ${(props) => props.theme['green-500']};
+  }
+
+  &::placeholder {
+    color: ${(props) => props.theme['gray-500']};
+  }
 `
 
 export const TaskInput = styled(BaseInput)`
