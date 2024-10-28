506487583bdcaf3283223573700ec255b7cfbece
saramargarido <saramargarido@gmail.com>
2024-10-28 22:09:47 +0100

    fix bugs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -50,12 +50,17 @@ export default function Home() {
   const activeCycle = cycles.find(cycle => cycle.id === activeCycleId)
 
   useEffect(() => {
+    let interval: number
     if (activeCycle) {
-      setInterval(() => {
+      interval = setInterval(() => {
         setAmountSecondsPassed(
           differenceInSeconds(new Date(), activeCycle.startDate),
         )
       }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval)
     }
   }, [activeCycle])
 
@@ -70,6 +75,7 @@ export default function Home() {
 
     setCycles(state => [...state, newCycle])
     setActiveCycleId(id)
+    setAmountSecondsPassed(0)
     reset()
   }
 
